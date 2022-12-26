65958e50821e2df4c5dfd7c4b66c00f9747a4a72
Nickolay Petrych <npetrych@alfabank.ru>
2022-12-26 16:47:14 +0500

    add Client class

--- src/test/java/socket/Client.java
+++ src/test/java/socket/Client.java
@@ -10,12 +10,12 @@ public class Client extends WebSocketClient {
     private final SocketContext context;
 
     public Client(URI serverUri, SocketContext context) throws URISyntaxException {
-        super( new URI(context.getURI()));
+        super(new URI(context.getURI()));
         this.context = context;
     }
 
     /**
-     *Open connection
+     * Open connection
      */
     @Override
     public void onOpen(ServerHandshake handshakedata) {
@@ -23,11 +23,15 @@ public class Client extends WebSocketClient {
     }
 
     /**
-     *Send massage
+     * Send massage
      */
     @Override
     public void onMessage(String message) {
-
+        System.out.println("Received new message " + message);
+        context.getMessageList().add(message);//сохраняем все новые сообщения в список
+        if (message.equals(context.getExpectedMessage())) {//завершаем подключение к сокету если ожидаемое сообщение пришло
+            closeConnection(1000, "Received expected message");
+        }
     }
 
     /**
@@ -35,11 +39,12 @@ public class Client extends WebSocketClient {
      */
     @Override
     public void onClose(int code, String reason, boolean remote) {
-
+        System.out.println("Close socket with code " + code + "reason is " + reason);
+        context.setStatusCode(code);
     }
 
     /**
-     *Exceptions
+     * Exceptions
      */
 
     @Override
